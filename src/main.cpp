023c1f75066a6ab9cc8de7708ce4ec44874aa6aa
David Nieto <davidnp@gmail.com>
2019-07-04 18:51:27 +0200

    Increase version v2.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,11 @@
 
 int main ( int argc, char *argv[] ) {
 	char headerLog[64];
-	sprintf(headerLog, "**** CMD-TOOL v%d.%d ****", cmd_tool_VERSION_MAJOR, cmd_tool_VERSION_MINOR);
+	sprintf(headerLog, "**** CMD-TOOL v%d.%d.%d ****", 
+			cmd_tool_VERSION_MAJOR,
+			cmd_tool_VERSION_MINOR,
+			cmd_tool_VERSION_PATCH);
+
 	string strHeaderLog = headerLog;
 	cout << headerLog << endl;
 
